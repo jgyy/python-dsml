725545d9a72fb6e05d85db5e7655718cffe3c876
jgyy <jeffrey.goh@sit.singaporetech.edu.sg>
2023-04-08 14:41:58 +0800

    init numpy

---
+++ 05/arrays.py
@@ -0,0 +1,6 @@
+'''This program demonstrates how to create and use arrays in Python.'''
+import numpy as np
+
+mylist = [1, 2, 3]
+arr = np.array(mylist)
+my_matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
